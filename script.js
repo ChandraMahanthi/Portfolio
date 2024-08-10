a2623a448b5766c59b7602be0c8d0934504caed3
Mr.DC/ <mahanthichandra9640@gmail.com>
2024-08-10 23:46:15 +0530

    added additional response submission

--- script.js
+++ script.js
@@ -21,8 +21,25 @@ form.addEventListener("submit",()=>
     let reply = document.createElement("div");
     // Styling for the reply box
     reply.style.width = "500px";
-    reply.style.height = "300px";
+    reply.style.height = "200px";
     reply.style.backgroundColor = "black";
-    reply.textContent = `Hey ${userName} thank you for filling the form. I'll contact you ASAP`;
+    reply.style.color = "gray";
+    reply.style.fontSize = "40px";
+    reply.style.textAlign = "center";
+    reply.innerHTML = `Hey <strong style="color:white">${userName}</strong> thank you for filling the form. I'll contact you ASAP`;
     section.append(reply);
+
+    let button = document.createElement("button");
+    button.innerHTML = `Create a new form`;
+    button.style.width = "200px";
+    button.style.height = "50px";
+    button.style.fontSize = "15px";
+    reply.append(button);
+
+    button.addEventListener("click",()=>
+    {
+        formBackground.style.display = "block"; 
+        formBackground.style.padding = "70px"; 
+        reply.style.display = "none" ; 
+    })
 })
