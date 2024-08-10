4b95866b26fb6af3b0263ae60d2dd05b856efa33
Mr.DC/ <mahanthichandra9640@gmail.com>
2024-08-10 18:41:55 +0530

    added response on submitting the form

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let Name = document.getElementById("name");
 let Contact = document.getElementById("phoneNumber");
-let form = document.querySelector("form")
+let form = document.querySelector("form");
+let section = document.getElementById("contact-section");
+let formBackground = document.getElementById("formBackground");
+
 
 form.addEventListener("submit",()=>
 {
@@ -8,11 +11,18 @@ form.addEventListener("submit",()=>
     console.log("Success");
     let userName = Name.value;
     let userContact = Contact.value;
-
+    
     let userData = {
         userName:userName,
         userContact:userContact
     };
     console.log(userData);
-
+    formBackground.style.display = "none";
+    let reply = document.createElement("div");
+    // Styling for the reply box
+    reply.style.width = "500px";
+    reply.style.height = "300px";
+    reply.style.backgroundColor = "black";
+    reply.textContent = `Hey ${userName} thank you for filling the form. I'll contact you ASAP`;
+    section.append(reply);
 })
